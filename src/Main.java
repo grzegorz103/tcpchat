66c888bd03155f893cdb4a634f09fdaf0e133152
grzegorz103 <38592759+grzegorz103@users.noreply.github.com>
2018-05-24 23:52:10 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,85 @@
+﻿import java.io.*;
+import java.net.Inet4Address;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+
+    private String nick;
+    private final static String HOST = "localhost";
+    private final static int PORT = 1500;
+    private InputStream is;
+    private OutputStream os;
+    private BufferedReader bis;
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        do {
+            System.out.println("Enter your nick");
+            main.nick = new Scanner(System.in).nextLine();
+            System.out.println(main.nick.length() <= 0 ? "Nick cannot be empty : "Welcome " + main.nick);
+        } while (main.nick.length() <= 0);
+
+        main.startChat();
+    }
+
+
+    private void startChat() throws IOException {
+
+        var socket = new Socket(HOST, PORT);
+        this.os = socket.getOutputStream();
+        this.is = socket.getInputStream();
+        var thread = new Thread(this::startListening);
+
+        thread.start();
+        start();
+    }
+
+    private void startListening() {
+
+        this.bis = new BufferedReader(new InputStreamReader(is));
+        while (true) {
+            try {
+                String line = this.bis.readLine();
+                if (line.equals("#")) {
+                    receiveFile();
+                } else {
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void receiveFile() throws IOException {
+        var fileName = bis.readLine();
+        FileOutputStream fos = new FileOutputStream("copy_" + fileName);
+        BufferedOutputStream fileBuffer = new BufferedOutputStream(fos);
+
+        var count = 0;
+        var data = new byte[2048];
+        while ((count = is.read(data)) > 0) {
+            fileBuffer.write(data, 0, count);
+        }
+
+        fileBuffer.close();
+        fos.close();
+        System.out.println("File " + fileName + " has been received.");
+        System.exit(0);
+    }
+
+    private void start() {
+        try {
+            os.write((this.nick + "\n").getBytes());
+
+            while (true) {
+                var line = new Scanner(System.in).nextLine();
+                os.write((this.nick + ": " + line + "\n").getBytes());
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,115 @@
+﻿import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+
+    private List<ClientThread> users = new ArrayList<>();
+    private ServerSocket serverSocket;
+    private Socket socket;
+
+    public static void main(String... args) throws IOException {
+        Server server = new Server();
+        server.serverSocket = new ServerSocket(1500);
+        while (true) {
+            server.socket = server.serverSocket.accept();
+            System.out.println("Client accepted");
+            ClientThread client = new ClientThread(server.socket, server::getUsers);
+            var thread = new Thread(client);
+            server.users.add(client);
+            thread.start();
+        }
+    }
+
+    private List<ClientThread> getUsers() {
+        return users;
+    }
+}
+
+@FunctionalInterface
+interface GetList {
+    List<ClientThread> getUserList();
+}
+
+class ClientThread implements Runnable {
+
+    private Socket socket;
+    private List<ClientThread> userList;
+    private OutputStream os;
+    private String nick;
+
+    ClientThread(Socket socket, GetList list) throws IOException {
+        this.socket = socket;
+        this.userList = list.getUserList();
+        this.os = socket.getOutputStream();
+    }
+
+    private void sendAll(String... message) {
+        this.userList.stream()
+                .filter(x -> !x.socket.isClosed())
+                .forEachOrdered(x -> x.send(message[0] + ": " + message[1] + "\n"));
+    }
+
+    private void send(String message) {
+        try {
+            os.write(message.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendToRecipient(String... message) {
+        this.userList.stream()
+                .filter(x -> x.nick.equals(message[1]))
+                .filter(x -> !x.socket.isClosed())
+                .forEachOrdered(x -> x.send("Message from: " + message[0] + ": " + message[1] + "\n"));
+    }
+
+    private void sendFile(String... message) {
+        this.userList.stream()
+                .filter(x -> x.nick.equals(message[1]))
+                .forEachOrdered(x -> x.startSending(message[3]));
+    }
+
+    private void startSending(String link) {
+        try (FileInputStream fileStream = new FileInputStream(link);
+             BufferedInputStream fileBuffer = new BufferedInputStream(fileStream)) {
+            os.write("#\n".getBytes());
+            os.write((link + "\n").getBytes());
+
+            var count = 0;
+            byte[] data = new byte[2048];
+            while ((count = fileBuffer.read(data)) > 0) {
+                os.write(data, 0, count);
+                os.flush();
+            }
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader bis = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            this.nick = bis.readLine();
+            while (true) {
+                String line = bis.readLine();
+                String[] temp = line.split(": ");
+                if (temp.length == 3) {
+                    sendToRecipient(temp);
+                } else if (temp.length == 4) {
+                    sendFile(temp);
+                } else {
+                    sendAll(temp);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
